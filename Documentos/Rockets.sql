fa78775a0e25564b237f33d6063a5359526ba46e
thiagoemidiosptech <thiago.souza@sptech.school>
2026-05-14 19:45:32 -0300

    sql

---
+++ Documentos/Rockets.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE rockets;
+
+USE rockets;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+id INT PRIMARY KEY AUTO_INCREMENT,
+titulo VARCHAR(100),
+descricao VARCHAR(150),
+fk_usuario INT,
+FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+insert into aquario (descricao) values ('Aquário de Estrela-do-mar');
+insert into aquario (descricao) values ('Aquário de Peixe-dourado');
+
+select * from usuario;
